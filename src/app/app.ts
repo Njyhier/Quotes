6aff60c04ad805e7b4a1e0695525e130fc91a638
Njyhier <njerinjihia43@gmail.com>
2025-10-18 21:55:45 +0300

    Add initial project structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputForm } from './input-form/input-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [InputForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/quote.ts
@@ -0,0 +1,6 @@
+export class Quote {
+  quote: string = '';
+  id: number = 0;
+  upVotes: number = 0;
+  downVotes: number = 0;
+}
